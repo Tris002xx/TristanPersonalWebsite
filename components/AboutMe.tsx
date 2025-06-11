cc356c0072de90236b6b50f99c796e9df9e7d483
tris002xx <tristanjames3131@gmail.com>
2025-06-11 11:13:00 -0700

    First Commit <Boilerplate code already added>

---
+++ components/AboutMe.tsx
@@ -0,0 +1,18 @@
+const AboutMe = () => {
+  return (
+    <div className="flex flex-col p-2">
+      <span className="space-grotesk-h1">Hello, I'm Tristan James Torres.</span>
+      <span className="space-grotesk-p">
+        A fresh Computer Science Graduate 💻 from British Columbia Institute of
+        Technology
+      </span>
+      <nav>
+        <a href="">GitHub</a>
+        <a href="">LinkedIn</a>
+        <a href="">Email</a>
+      </nav>
+    </div>
+  );
+};
+
+export default AboutMe;
---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <nav>
+      <menu className="flex gap-2 p-2 bg-gray-200 text-black">
+        <span>Logo</span>
+        <span>About me</span>
+        <span>Projects</span>
+        <span>Skills</span>
+        <span>Contact me</span>
+      </menu>
+    </nav>
+  );
+};
+
+export default Header;
