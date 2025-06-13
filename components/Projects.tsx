01ea7ba350e8b2158a94873cd92aa280ef8d875c
tris002xx <tristanjames3131@gmail.com>
2025-06-12 23:07:30 -0700

    first attempt to fix icon and title

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,12 +3,12 @@ import { useState } from "react";
 
 const picturesChess: string[] = [
   "/TristanPersonalWebsite/crop_chess.JPG",
-  "/2025-06-12 16-22-55.mp4",
+  "/TristanPersonalWebsute/2025-06-12 16-22-55.mp4",
 ];
 
 const picturesThreadit: string[] = [
   "/TristanPersonalWebsite/crop.JPG",
-  "/2025-06-12 17-31-01.mp4",
+  "/TristanPersonalWebsute/2025-06-12 17-31-01.mp4",
 ];
 
 const projectItems = [
